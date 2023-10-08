d778e44d841426d0c00a2662ecff6a59d0144ac2
NaysKutzu <ghermancassian2008@gmail.com>
2023-10-08 20:03:11 +0000

    PUSH -> Removed apache2 now using nginx -> Added the configs to nginx

--- install.bash
+++ install.bash
@@ -6,7 +6,6 @@ echo "For support join our community: https://discord.gg/7BZTmSK2D8"
 
 sleep 1s
 echo ""
-# Check if the user is root and if this is an actual user.
 if [ "$(id -u)" != "0" ]; then
     printf "This script must be run as root\nYou can login as root with\033[0;32m sudo su -\033[0m\n" 1>&2
     exit 1
@@ -14,25 +13,21 @@ fi
 read -p "Are you sure you want to continue? [y/n] " installation
 if [[ $installation == "y" || $installation == "Y" || $installation == "yes" || $installation == "Yes" ]]
 then
-    # Update packages
     sudo apt update -y && sudo apt upgrade -y
-    # Install Dependency
-    sudo apt --ignore-missing install apache2 git docker containerd docker.io docker-compose -y
-    # Moving in /etc
+    sudo apt --ignore-missing install nginx git docker containerd docker.io docker-compose -y
     cd /etc
-    # Clone stuff
     sudo git clone https://github.com/MythicalLTD/KosmaPanel-Daemon.git KosmaPanel
     sudo mv /etc/KosmaPanel/daemon.service /etc/systemd/system/
     sudo systemctl enable --now daemon.service
     bash arch.bash
-    echo "Installing apache2 config..."
+    echo "Installing nginx config..."
 	clear
     read -p 'Enter your domain ( No IP ): '  domain
-    service apache2 stop
+    systemctl stop nginx
     certbot certonly --standalone -d $domain
-    cp /etc/KosmaPanel/daemon.conf /etc/apache2/sites-available/daemon.conf
-    sed -i "s/url/$domain/" /etc/apache2/sites-available/daemon.conf
-    ln -s /etc/apache2/sites-available/daemon.conf /etc/apache2/sites-enabled/daemon.conf
+    cp /etc/KosmaPanel/kosmapanel-daemon.conf /etc/nginx/sites-available/kosmapanel-daemon.conf
+    sed -i "s/url/$domain/" /etc/nginx/sites-available/kosmapanel-daemon.conf
+    ln -s /etc/nginx/sites-available/kosmapanel-daemon.conf /etc/nginx/sites-enabled/kosmapanel-daemon.conf
     rm -r /var/www/html
     mkdir -p /var/www/html
     mkdir -p /var/www/html/errors
@@ -44,16 +39,11 @@ then
     cp /etc/KosmaPanel/templates/503.html /var/www/html/errors
     chown -R www-data:www-data /var/www/html/*
     sudo chmod -R 755 /var/www/html/*
-    rm /etc/apache2/sites-available/000-default.conf
-    rm /etc/apache2/sites-enabled/000-default.conf
-    cp /etc/KosmaPanel/000-default.conf /etc/apache2/sites-available/000-default.conf
-    ln -s /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-enabled/000-default.conf
-    sudo a2enmod proxy
-    sudo a2enmod proxy_http
-    sudo a2enmod proxy_wstunnel
-    sudo a2enmod rewrite
-    sudo a2enmod ssl
-    service apache2 start
+    rm /etc/nginx/sites-available/default
+    rm /etc/nginx/sites-enabled/default
+    cp /etc/KosmaPanel/default.conf /etc/nginx/sites-available/default.conf
+    ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
+    systemctl start nginx
     echo ""
     echo " --> Installation completed"
     echo ""
